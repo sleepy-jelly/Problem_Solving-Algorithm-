f821b00df175109ced4d1779acfb59b3699deff9
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 15:03:22 +0900

    [Bronze II] Title: 16진수, Time: 204 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        
+       Scanner sc = new Scanner(System.in);
+
+       String nData1 = sc.nextLine();
+       int nInt1 = Integer.parseInt(nData1,16);
+
+
+
+
+       System.out.println(nInt1);
+
+        sc.close();
+    }        
+}
+     
