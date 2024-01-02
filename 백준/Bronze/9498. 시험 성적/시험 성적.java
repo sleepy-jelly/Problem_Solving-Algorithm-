56a5e853e42c0c30889027e02b3c012250ee36a0
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 15:17:09 +0900

    [Bronze V] Title: 시험 성적, Time: 212 ms, Memory: 17712 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner UserTextInput1 = new Scanner(System.in)) {			
+			int a=UserTextInput1.nextInt();
+			if(a>=90) {
+				System.out.println("A");
+			}else if(a>=80) {
+				System.out.println("B");
+			}
+			else if(a>=70) {
+				System.out.println("C");
+			}
+			else if(a>=60) {
+				System.out.println("D");
+			}
+			else {
+				System.out.println("F");
+			}
+		}
+
+	}
+
+}
