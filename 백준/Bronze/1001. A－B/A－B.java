6a2d48178b5a8e037478a8ac86ab95e077d39c51
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 14:26:45 +0900

    [Bronze V] Title: A-B, Time: 228 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main{
+	public static void main(String[]args) {
+
+try (Scanner UserTextInput1 = new Scanner(System.in)) {			
+			int a=UserTextInput1.nextInt();
+			int b=UserTextInput1.nextInt();
+			
+			System.out.println(a-b);
+			}		
+		}
+}		  
