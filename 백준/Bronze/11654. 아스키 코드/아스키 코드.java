ef865461c4adb82b63ffb8a8af61996d485cea22
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 14:47:46 +0900

    [Bronze V] Title: 아스키 코드, Time: 196 ms, Memory: 17568 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);;
+		int a = sc.next().charAt(0);
+	
+		sc.close();
+		
+		System.out.println(a);
+		
+		
+		
+	}
+}
