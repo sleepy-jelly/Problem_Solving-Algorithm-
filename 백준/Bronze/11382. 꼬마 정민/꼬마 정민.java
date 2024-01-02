6a64fe905cd3fd46395a735f4395956bfe39f662
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 14:29:10 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+
+        long nData1,nData2,nData3;
+
+       nData1=sc.nextLong();
+
+        nData2=sc.nextLong();
+        
+        nData3=sc.nextLong();
+
+
+        System.out.println(nData1+nData2+nData3);
+
+
+
+        sc.close();
+
+
+    }
+
+
+}
