90cdbb7aa1522215c82d594517c4f042c5bf0004
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 15:07:26 +0900

    [Bronze V] Title: N 찍기, Time: 960 ms, Memory: 29220 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader; //BufferedReader kind of Similar to Scanner
+import java.io.BufferedWriter; //BufferedWriter kind of Similar to println
+import java.io.IOException;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader nBufferReader1 = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter nBufferWriter1 = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(nBufferReader1.readLine());
+		
+		
+		for (int i = 1; i < N+1; i++) {
+			System.out.println(i);
+		}
+		nBufferReader1.close();
+		nBufferWriter1.flush();
+		nBufferWriter1.close();
+
+	}
+
+}
