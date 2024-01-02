94542c9c81ac6cb3564cceaf62c0d3d15ba21575
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 15:03:30 +0900

    [Bronze II] Title: 손익분기점, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        if(C<=B) {
+            System.out.println(-1);
+        }
+        else {
+           System.out.println((A/(C-B))+1);
+        }
+
+
+
+        }
+    }
+
