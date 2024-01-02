894d3be40068d0516b9e51f10d87363a6c88db1b
sleepy-jelly <57306207+sleepy-jelly@users.noreply.github.com>
2024-01-02 15:24:35 +0900

    [Bronze V] Title: 연세대학교, Time: 200 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int nData1= sc.nextInt();
+
+
+        if(nData1==0){
+            System.out.println("YONSEI");
+        }else{
+            System.out.println("Leading the Way to the Future");
+        }
+
+
+
+
+    }
+}
